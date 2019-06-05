8013922e692906d0ef0fa05e67dc308c09ba0f40
gmp26 <gmp26@cam.ac.uk>
2019-06-05 11:12:44 +0100

    added project.clj description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject winton-utils "0.2.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Winton centre utilities"
+  :url "https://github.com/WintonCentre/winton-utils"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
